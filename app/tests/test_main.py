73ec5da9d8a78ed8640ee8d750140c64aa222510
Андрій Тимофійчук <hok.ykrop@gmail.com>
2024-07-12 20:13:12 +0300

    1.1 One test was added, uvicorn added, proper structure added.

---
+++ app/tests/test_main.py
@@ -0,0 +1,9 @@
+import pytest
+from httpx import AsyncClient
+from app.main import app
+
+@pytest.mark.asyncio
+async def test_read_root():
+    async with AsyncClient(app=app, base_url="http://health_check") as ac:
+        response = await ac.get("http://127.0.0.1:8000/")
+    assert response.json() == {"status_code": 200, "detail": "ok", "result": "working"}
