e22b2badca900f15bc8c662898da04a604d39817
Андрій Тимофійчук <hok.ykrop@gmail.com>
2024-07-14 02:00:46 +0300

    3.1 docker-compose.yml was changed, now it connects to redis and postgres databases. base file was added for interaction with databases in the future.

--- config.py
+++ config.py
@@ -5,6 +5,16 @@ class Settings(BaseSettings):
     uvicorn_host: str
     uvicorn_port: int
 
+    postgresql_host: str
+    postgresql_port: int
+    postgresql_database_name: str
+    postgresql_password: str
+    postgresql_user: str
+
+    redis_host: str
+    redis_port: int
+    redis_pass: str
+
     model_config = SettingsConfigDict(env_file=dotenv.find_dotenv(), extra = "allow")
 
 settings = Settings()
