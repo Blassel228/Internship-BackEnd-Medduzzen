cafe06a498e62b8f2e6bd149d5c187a265a1bfba
Serhii Honcharov <sergeygoncharov9@gmail.com>
2024-10-27 19:21:14 +0200

    (github-actions) github-actions-ec2.yml was changed to delete files not present in my branch and copying not existing files, at the same time rewriting existing ones.

--- create_env.sh
+++ create_env.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Create .env file
-echo "POSTGRES_HOST=${POSTGRES_HOST}" > /code/.env
-echo "POSTGRES_PORT=${POSTGRES_PORT}" >> /code/.env
-echo "POSTGRES_DB=${POSTGRES_DB}" >> /code/.env
-echo "POSTGRES_USER=${POSTGRES_USER}" >> /code/.env
-echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> /code/.env
-echo "REDIS_HOST=${REDIS_HOST}" >> /code/.env
-echo "REDIS_PORT=${REDIS_PORT}" >> /code/.env
-echo "REDIS_PASSWORD=${REDIS_PASSWORD}" >> /code/.env
-echo "SECRET=${SECRET}" >> /code/.env
-echo "ALGORITHM=${ALGORITHM}" >> /code/.env
-echo "DOMAIN=${DOMAIN}" >> /code/.env
-echo "API_AUDIENCE=${API_AUDIENCE}" >> /code/.env
+echo "POSTGRES_HOST=${POSTGRES_HOST}" > /code/.env && \
+echo "POSTGRES_PORT=${POSTGRES_PORT}" >> /code/.env && \
+echo "POSTGRES_DB=${POSTGRES_DB}" >> /code/.env && \
+echo "POSTGRES_USER=${POSTGRES_USER}" >> /code/.env && \
+echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> /code/.env && \
+echo "REDIS_HOST=${REDIS_HOST}" >> /code/.env && \
+echo "REDIS_PORT=${REDIS_PORT}" >> /code/.env && \
+echo "REDIS_PASSWORD=${REDIS_PASSWORD}" >> /code/.env && \
+echo "SECRET=${SECRET}" >> /code/.env && \
+echo "ALGORITHM=${ALGORITHM}" >> /code/.env && \
+echo "DOMAIN=${DOMAIN}" >> /code/.env && \
+echo "API_AUDIENCE=${API_AUDIENCE}" >> /code/.env && \
 echo "ISSUER=${ISSUER}" >> /code/.env
