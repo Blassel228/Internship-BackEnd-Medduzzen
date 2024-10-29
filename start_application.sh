3574f99994a2adcd19cd3c57dd83dcee8f60290e
Serhii Honcharov <sergeygoncharov9@gmail.com>
2024-10-29 21:51:56 +0200

    (github-actions change) changes made as autodeploy has fallen for a while

--- start_application.sh
+++ start_application.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 set -e
 
+REDIS_URL="redis://redis.bdajto.ng.0001.use1.cache.amazonaws.com:6379/0"
+
 function start_celery_worker() {
     echo "Starting Celery worker..."
-    celery -A app.celery_app:app --broker redis://redis.bdajto.ng.0001.use1.cache.amazonaws.com:6379/0 --result-backend redis://redis.bdajto.ng.0001.use1.cache.amazonaws.com:6379/0 worker --pool=solo --loglevel=info -E &
+    exec celery -A app.celery_app:app --broker "$REDIS_URL" --result-backend "$REDIS_URL" worker --pool=solo --loglevel=info -E &
     WORKER_PID=$!
 }
 
 function start_celery_beat() {
     echo "Starting Celery beat..."
-    celery -A app.celery_app:app --broker redis://redis.bdajto.ng.0001.use1.cache.amazonaws.com:6379/0 --result-backend redis://redis.bdajto.ng.0001.use1.cache.amazonaws.com:6379/0 beat --loglevel=info &
+    exec celery -A app.celery_app:app --broker "$REDIS_URL" --result-backend "$REDIS_URL" beat --loglevel=info &
     BEAT_PID=$!
 }
 
 function start_uvicorn() {
     echo "Starting Uvicorn server..."
-    uvicorn app.main:app --host 0.0.0.0 --port 8002 &
+    exec uvicorn app.main:app --host 0.0.0.0 --port 8002 &
     UVICORN_PID=$!
 }
 
 function stop_services() {
-    echo "Stopping Uvicorn server..."
-    kill $UVICORN_PID
-    echo "Stopping Celery beat..."
-    kill $BEAT_PID
-    echo "Stopping Celery worker..."
-    kill $WORKER_PID
+    echo "Stopping services..."
+    kill $UVICORN_PID $BEAT_PID $WORKER_PID
 }
 
 trap stop_services SIGTERM SIGINT
